fd1048e6318fc89ec0961b7a9fd3b0c94c24edc7
Noam <noamfishi@gmail.com>
2022-01-09 21:10:02 +0200

    Adding class time content

--- mongoDB/class time content/models/Student.js
+++ mongoDB/class time content/models/Student.js
@@ -3,14 +3,26 @@ const mongoose = require("mongoose");
 const studentSchema = new mongoose.Schema({
   name: {
     type: String,
+    lowercase: true,
+    maxLength: 20,
     default: "Noam",
   },
   age: {
     type: Number,
-    max: 120,
+    max: 130,
+    min: 0,
     required: true,
   },
-  date: Date,
+  date: {
+    type: Date,
+    default: () => new Date().getTime(),
+  },
+  test: [
+    {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "Test",
+    },
+  ],
 });
 
 module.exports = mongoose.model("Student", studentSchema);
--- mongoDB/class time content/queriesPart2.js
+++ mongoDB/class time content/queriesPart2.js
@@ -2,6 +2,7 @@
 const mongoose = require("mongoose");
 const key = require("./secret").MONGO_KEY;
 const {studentSchema} = require("./models/Student");
+const {testsSchema} = require("./models/Tests");
 
 async function getConnection() {
   try {
@@ -11,16 +12,32 @@ async function getConnection() {
   }
 }
 
+async function createTest() {
+  try {
+    const connection = await getConnection();
+    const Test = connection.model("Test", testsSchema);
+    const test = new Test({score: 85, category: "math"});
+    console.log(test);
+
+    test
+      .save()
+      .then(() => connection.disconnect())
+      .then(console.log("Done!"));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 async function create() {
   try {
     const connection = await getConnection();
     const Student = connection.model("Student", studentSchema);
-    const student = new Student({name: "Riki", age: 119});
+    const student = new Student({name: "Ynon", age: 20, test: "61db258f8f1a042130eec117"});
     console.log(student);
 
     student
       .save()
-      .then(() => connection.disconnect())
+      .then((res) => connection.disconnect())
       .then(console.log("Done!"));
   } catch (e) {
     console.log(e);
@@ -53,6 +70,24 @@ async function findOneAndUpdate() {
     console.log(e);
   }
 }
-create();
+async function findStudent() {
+  try {
+    const nameKuku = "Ynon";
+    const connection = await getConnection();
+    const Student = connection.model("Student", studentSchema);
+    const students = await Student.find({name: "ynon"}).populate("test");
+    const students2 = await Student.where(nameKuku).equals("Ynon").populate("test").limit(2);
+    const students3 = await Student.where("age").gt("40").where("name").equals("noam").limit(2).select("date");
+
+    console.log(students);
+    await connection.disconnect();
+    console.log("Done!");
+  } catch (e) {
+    console.log(e);
+  }
+}
+// createTest();
+// create();
 // find();
 // findOneAndUpdate();
+findStudent();
